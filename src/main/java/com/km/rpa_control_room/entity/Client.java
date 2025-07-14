67c32b2a2f0a9b06ece6e40a7df0fd69798ceb6e
Karl Mulraney <kpmulraney@gmail.com>
2025-07-14 12:25:25 +0100

    Beginning addition of 'Client' entity (one to many mapping)

---
+++ src/main/java/com/km/rpa_control_room/entity/Client.java
@@ -0,0 +1,28 @@
+package com.km.rpa_control_room.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "client")
+public class Client {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(name = "name")
+   private String name;
+
+   @Column(name = "vm_address")
+   private String vmAddress;
+
+   @Column(name = "bots")
+   private List<Bot> bots;
+}
